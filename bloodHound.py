0d808a8bc5bac93716f480a637f0cff49a88226f
Rushill Shah <rushill2@illinois.edu>
2023-02-22 15:06:54 -0600

    Refactor dirs

--- bloodHound.py
+++ bloodHound.py
@@ -36,10 +36,18 @@ SOURCE, DEST, LEN, CHKSUM = struct.unpack("! H H H H", data[:8])
 '''
 # RAW packets like the ones we get with the above command contain everything except the checksum
 # payloads smaller than 46B are padded
+import socket
+import struct
 
 def unpack_ethernet_frame(data):
     dest_mac, src_mac, ethertype = struct.unpack('! 6s 6s H', data[:14])
     return dest_mac, src_mac, ethertype, data[14:]
+
+def mac_to_str(data):
+    octets = []
+    for b in data:
+        octets.append(format(b, '02x'))
+    return "-".join(octets)
 
 ETH_P_ALL = 0x03 # Listen for everything
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
@@ -47,12 +55,4 @@ s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
 while True:
     raw_data, addr = s.recvfrom(65565)
     dest_mac, src_mac, ethertype, payload = unpack_ethernet_frame(raw_data)
-    print(f"[ Frame - Dest: {dest_mac}; Source: {src_mac}; EtherType: {hex(ethertype)} ]")
-
-s.bind(("eth0", 0))
-
-# Print basic socket info
-print(s.getsockname())          # ('eth0', 3, 0, 1, b"\x08\x00'~\x88\x1f")
-
-# Print all available interfaces
-print(socket.if_nameindex())    # [(1, 'lo'), (2, 'eth0')]
+    print(f"[ Frame - Dest: {mac_to_str(dest_mac)}; Source: {mac_to_str(src_mac)}; EtherType: {hex(ethertype)} ]")
